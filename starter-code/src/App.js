cd33a4f4faafcf152ee08449983a82287b841bd7
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-05-05 16:46:51 +0200

    first iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import usersData from "./users";
 
 class App extends React.Component {
-
+  state = {};
 
   render() {
-    (
-      < div ></div >
+    const mappedUsersData = usersData.map((user) => (
+        <tr key={user.lastName}>
+          <td>{user.firstName}</td>
+          <td>{user.lastName}</td>
+          <td>{user.campus}</td>
+          <td>{user.role}</td>
+          <td>{user.links}</td>
+        </tr>
+    ));
+    return (
+      <div className="App">
+        <table>
+        <thead>
+          <tr>USERS</tr>
+        </thead>
+          <tr>
+            <td>first name</td>
+            <td>last name</td>
+            <td>campus</td>
+            <td>role</td>
+            <td>links</td>
+          </tr>
+          {mappedUsersData}
+        </table>
+      </div>
     );
   }
 }
