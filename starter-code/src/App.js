08bc26eff14693cf92aad6b03ee2e7e34d5ba055
alexanderrathbone <alexander.thomas.rathbone@hotmail.com>
2021-05-05 19:05:19 +0200

    iteration 2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,34 +1,91 @@
 import React from "react";
 import "./App.css";
-import usersData from "./users";
+import usersDB from "./users";
+
+const usersData = JSON.parse(JSON.stringify(usersDB));
 
 class App extends React.Component {
-  state = {};
+  state = {
+    users: usersData,
+    search: "",
+  };
+
+  searchHandler = (event) => {
+    var target = event.target.value.toLowerCase();
+    this.setState({
+      search: target.replace(/ /g, ""),
+    });
+    
+    console.log(this.state.search);
+  };
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    // var target = event.target.value.toLowerCase();
+    // this.setState({
+    //   search: target
+    // });
+    // console.log(this.state.search)
+    const dataCopy = JSON.parse(JSON.stringify(usersData));
+    // const searchResult = [];
+    const searchResult = dataCopy.filter(user=>{
+      return user.firstName.toLowerCase().includes(this.state.search.toLocaleLowerCase())
+    })
+
+    // for (let i = 0; i < dataCopy.length; i++) {
+    //   if (dataCopy[i].firstName.includes(String(this.state.search))) {
+    //     searchResult.push(dataCopy[i]);
+    //   }
+    // }
+    console.log('this',searchResult)
+    this.setState({
+      users : searchResult
+    })
+  };
 
   render() {
-    const mappedUsersData = usersData.map((user) => (
-        <tr key={user.lastName}>
-          <td>{user.firstName}</td>
-          <td>{user.lastName}</td>
-          <td>{user.campus}</td>
-          <td>{user.role}</td>
-          <td>{user.links}</td>
-        </tr>
+    const mappedUsersData = this.state.users.map((user, i) => (
+      // const mappedUsersData = this.state.users.map((user, i) => (
+      <tr key={i}>
+        <td>{user.firstName}</td>
+        <td>{user.lastName}</td>
+        <td>{user.campus}</td>
+        <td>{user.role}</td>
+        <td>{user.linkedin}</td>
+      </tr>
     ));
     return (
       <div className="App">
+        <form onSubmit={this.submitHandler}>
+          <label htmlFor="search">search</label>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            onChange={this.searchHandler}
+          />
+        </form>
         <table>
-        <thead>
-          <tr>USERS</tr>
-        </thead>
-          <tr>
-            <td>first name</td>
-            <td>last name</td>
-            <td>campus</td>
-            <td>role</td>
-            <td>links</td>
-          </tr>
-          {mappedUsersData}
+          <tbody>
+            <tr>
+              <td>
+                <p>first name</p>
+              </td>
+              <td>
+                <p>last name</p>
+              </td>
+              <td>
+                <p>campus</p>
+              </td>
+              <td>
+                <p>role</p>
+              </td>
+              <td>
+                <p>links</p>
+              </td>
+            </tr>
+            {mappedUsersData}
+          </tbody>
         </table>
       </div>
     );
